7b4ab26f2e3b49b233702ede8e7ac5ff04f5d74a
DevNguyenPhuong <dev.nguyenphuong@gmail.com>
2023-11-17 19:03:18 +0700

    completed respondsive for iphone XR

--- src/features/tasks/CreateTaskForm.jsx
+++ src/features/tasks/CreateTaskForm.jsx
@@ -75,7 +75,7 @@ function CreateTaskForm({ onCloseModal }) {
 
       <FormRow label="Priority" error={errors?.priority?.message}>
         <select
-          className="rounded-md border border-solid border-gray-300 bg-gray-50 px-5 py-3 shadow-[0_1px_2px_rgba(0,0,0,0.4)]"
+          className="rounded-md border border-solid border-gray-300 bg-gray-50  px-5 py-1 shadow-[0_1px_2px_rgba(0,0,0,0.4)] md:py-3"
           disabled={isCreating}
           id="priority"
           {...register("priority", {
@@ -94,7 +94,7 @@ function CreateTaskForm({ onCloseModal }) {
 
       <FormRow label="Description" error={errors?.description?.message}>
         <textarea
-          className="h-[10rem] w-[20rem] rounded-md border border-solid border-gray-300 bg-gray-50 px-5 py-3 shadow-[0_1px_2px_rgba(0,0,0,0.4)]"
+          className="h-[5rem] w-[20rem] rounded-md border border-solid border-gray-300 bg-gray-50 px-5 py-3 shadow-[0_1px_2px_rgba(0,0,0,0.4)] md:h-[10rem] md:w-[20rem]"
           disabled={isCreating}
           id="description"
           maxLength={150}
@@ -112,14 +112,14 @@ function CreateTaskForm({ onCloseModal }) {
         <button
           variation="secondary"
           type="reset"
-          className="rounded-lg border border-solid border-gray-200 bg-gray-50 px-5 py-5 text-[1.4rem] font-[500] text-gray-600 hover:bg-gray-100"
+          className="rounded-lg border border-solid border-gray-200 bg-gray-50 px-2 py-2 text-[0.8rem] font-[500] text-gray-600 hover:bg-gray-100 md:px-5 md:py-5 md:text-[1.4rem]"
         >
           Cancel
         </button>
 
         <button
           disabled={isCreating}
-          className="flex flex-col items-center rounded-md bg-indigo-600 px-10 py-5 text-[1.6rem] font-medium text-indigo-50 hover:bg-indigo-500"
+          className="flex flex-col items-center rounded-md bg-indigo-600 px-5 py-2 text-[1rem] font-medium text-indigo-50 hover:bg-indigo-500 md:px-10 md:py-5 md:text-[1.6rem]"
         >
           Add task
         </button>
